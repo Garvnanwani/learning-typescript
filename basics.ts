f9b75bcf0356c1aedae10e68fd42dc5bb19fbccb
Garv <garvwashere@gmail.com>
2021-05-16 16:50:01 +0530

    added basic types

---
+++ basics.ts
@@ -0,0 +1,12 @@
+// basic types
+
+let userName: string = "garv"
+let hasLoggedIn: boolean = true
+let myNumber: number = 15
+
+userName += " nanwani"
+
+console.log(userName)
+
+// regex type
+let myRegex = /foo/
