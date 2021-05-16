186159ece2ec8957b10aa338604b9364547323d9
Garv <garvwashere@gmail.com>
2021-05-16 17:34:31 +0530

    📦 NEW: deep dive into function types

--- functions/functions-test.ts
+++ functions/functions-test.ts
@@ -3,3 +3,4 @@ import addNumbers, { addStrings } from "./functions";
 console.log(addNumbers(1, 2))
 // console.log(addNumbers(1,"garv")) wont work
 console.log(addStrings("a", 'b'))
+console.log(addStrings("a"))
--- functions/functions.ts
+++ functions/functions.ts
@@ -3,10 +3,35 @@
 function addNumbers(a: number, b: number): number {
     return a + b
 }
-
-// arrow function in ts
-export const addStrings = (str1: string, str2: string): string => `${str1} ${str2}`
-
-
 // you dont have module.exports in ts
 export default addNumbers;
+
+// arrow function in ts
+// also default value is shown
+export const addStrings = (str1: string, str2: string = ""): string => {
+    return `${str1} ${str2}`
+}
+
+
+// Union types
+// sometimes you want to have a variable or a function take two or more different types and unions help there
+
+export const format = (title: string, param: string | number): string => {
+    return `${title} ${param}`
+}
+
+// void type is used when function doesnt returns something
+export const printFormat = (title: string, param: string | number): void => {
+    console.log(format(title, param))
+}
+
+// Promise type
+export const fetchData = (url: string): Promise<string> => {
+    return Promise.resolve(`Data from ${url}`)
+}
+
+
+// rest parameters
+export const introduce = (salutation: string, ...names: string[]): string => {
+    return `${salutation} ${names.join(" ")}`
+}
