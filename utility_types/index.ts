d5c8598d114f39c6977a2aee3bbfb81efa320800
Garv <garvwashere@gmail.com>
2021-05-18 15:55:55 +0530

    added more utility types

--- utility_types/index.ts
+++ utility_types/index.ts
@@ -29,3 +29,45 @@ console.log(merge({
 }, {
     email: "something@gmail.com"
 }));
+
+
+// the opposite of partial is required
+
+type RequiredMyUser = Required<MyUser> // all fields become required
+
+
+// choosing the fields
+
+type JustEmailAndName = Pick<MyUser, "email" | "name"> // you can pick the fields you want, optionals are kept optional
+
+
+// creating a record
+
+// the opp of pick is omit, which lets you remove certain fields from type or interface
+
+type UserWithoutId = Omit<MyUser, "id">
+
+// one more thing you can do is taking the type from the field rather than typing it directly so instead of type string you can use type of id by MyUser["id"]
+
+const mapById = (users: MyUser[]): Record<MyUser["id"], UserWithoutId> => {
+
+    return users.reduce((a, v) => {
+        const { id, ...other } = v
+
+        return {
+            ...a,
+            [id]: other
+        }
+    }, {})
+}
+
+console.log(mapById([
+    {
+        id: "foo",
+        name: "some name"
+    },
+    {
+        id: "foo2",
+        name: "some other name"
+    }
+]));
